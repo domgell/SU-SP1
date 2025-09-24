bf9eb377efbfc1d179f601d51c5f12dfcd63e99c
PontusBontus <230225290+pontusebeling@users.noreply.github.com>
2025-09-24 17:55:54 +0200

    små fix

--- Assets/Scripts/Scenes/MenuController.cs
+++ Assets/Scripts/Scenes/MenuController.cs
@@ -3,13 +3,13 @@ using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
-    [SerializeField] private Button playButton;
+
     [SerializeField] private Button quitButton;
 
 
     private void Start()
     {
-        playButton.onClick.AddListener(() => { UnityEngine.SceneManagement.SceneManager.LoadScene("Level1"); });
+
         quitButton.onClick.AddListener(() => { Application.Quit(); });
     }
 }
